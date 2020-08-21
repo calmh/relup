c8eaa18202c17a17eb1062ba44692d1a98cc8d79
Jakob Borg <jakob@kastelo.net>
2020-08-21 13:29:44 +0200

    Remove progress writer

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -65,23 +64,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fi, _ := fd.Stat()
 
 	info, _ := fd.Stat()
 	size := info.Size()
-	progr := &progressWriter{
-		expected: size,
-	}
-	tee := io.TeeReader(fd, progr)
 
 	log.Println("Uploading", path.Base(file))
 	url := strings.Replace(uploadURL, "{?name,label}", "?name="+path.Base(file), 1)
-	req, err = http.NewRequest("POST", url, tee)
+	req, err = http.NewRequest("POST", url, fd)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	req.ContentLength = fi.Size()
+	req.ContentLength = size
 	req.Header.Set("Content-Type", "application/octet-stream")
 	req.Header.Set("Authorization", "token "+token)
 
@@ -90,27 +84,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	progr.Close()
-	log.Println(resp.Status)
-}
-
-type progressWriter struct {
-	expected int64
-	current  int64
-}
-
-func (p *progressWriter) Write(data []byte) (int, error) {
-	p.current += int64(len(data))
-	p.printProgress()
-	return len(data), nil
-}
-
-func (p *progressWriter) Close() error {
-	fmt.Println()
-	return nil
-}
-
-func (p *progressWriter) printProgress() {
-	pct := float64(p.current) / float64(p.expected) * 100
-	fmt.Printf("\r%d / %d (%5.01f%%)", p.current, p.expected, pct)
+	log.Printf("Completed %s: %v", path.Base(file), resp.Status)
 }
